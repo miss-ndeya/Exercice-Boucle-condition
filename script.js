41e6bf053a500b6e2323f7ebebac47df01f99074
unknown <miss-ndeya>
2023-05-25 22:39:31 +0200

    deploiment

---
+++ script.js
@@ -0,0 +1,45 @@
+                            /*****exercice1*****/
+/*
+// ecrire une boucle for qui affiche les nombres de 1 a 10.
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+*/
+
+                            /*****exercice2*****/
+/*                           
+// ecrire une boucle while qui affiche les nombres les nombres pairs de 0 a 20 dans la console
+let i = 0;
+while (i <= 20) {
+    console.log(i)
+    i = i +2
+}
+*/
+
+                            /*****exercice3*****/
+/*
+function nombre(x) {
+  for (let i = 0; i <= x; i++) {
+    if (i % 2 === 0) {
+      console.log(i, 'pair');
+    } else {
+      console.log(i, 'impair');
+    }
+  }
+}
+nombre(5);
+*/
+
+
+                            /*****exercice4*****/
+
+// ecrire une boucle for qui calcul la somme des nombres de 1 a 100 puis fiche le resultat dans la console
+let x = 100; 
+let somme = 0; // Variable pour stocker la somme
+
+for (let i = 1; i <= x; i++) {
+  somme += i;
+}
+console.log(`La somme des nombres de 1 a ${x} est ${somme}`);
+
+                            /********exercice5********/
