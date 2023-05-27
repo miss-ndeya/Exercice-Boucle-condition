2b9ac9db221193a81916235dc7d01ec66ae7cc61
unknown <miss-ndeya>
2023-05-27 23:34:37 +0200

    ressayage

--- script.js
+++ script.js
@@ -7,39 +7,72 @@ for (let i = 1; i <= 10; i++) {
 */
 
                             /*****exercice2*****/
-/*                           
-// ecrire une boucle while qui affiche les nombres les nombres pairs de 0 a 20 dans la console
+ /*                          
+// ecrire une boucle while qui affiche les nombres pairs de 0 a 20 dans la console
 let i = 0;
 while (i <= 20) {
-    console.log(i)
-    i = i +2
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+  i += 1;
 }
-*/
-
-                            /*****exercice3*****/
+ */                           /*****exercice3*****/
 /*
+// ecrire une fonction qui prend un nombre en parametre et affiche "nombre pair" si le nombre est pair, et "nombre pair" si le nombre est impair
 function nombre(x) {
-  for (let i = 0; i <= x; i++) {
-    if (i % 2 === 0) {
-      console.log(i, 'pair');
+    if (x % 2 === 0) {
+      console.log('nombre pair');
     } else {
-      console.log(i, 'impair');
+      console.log('nombre impair');
+    }
+}
+nombre(5);*/
+                            /*****exercice4*****/
+/*
+//  ecrire une boucle for qui calcul la somme des nombres de 1 a 100 puis affiche le resultat dans la console   
+let somme = 0; 
+for (let i = 1; i <= 100; i++) {
+  somme += i; 
+} 
+console.log(`La somme des nombres de 1 a 100 est ${somme}`);
+*/
+                            /********exercice5********/
+/*
+// ecrire une fonction qui prend un tableau de nombre en parametre et retourne la somme de tous les nombres pairs du tableau
+let nombres = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+function fonctionpair(tableau) {
+  let somme = 0;
+  for (let i = 0; i < tableau.length; i++) {
+    if (tableau[i] % 2 === 0) {
+      somme += tableau[i]
     }
   }
+  return somme;
 }
-nombre(5);
+let resultat = fonctionpair(nombres);
+console.log(resultat);  
 */
+                          /********exercice6********/
 
+// ecrire une boucle for qui itere de 1 a 20. Pour chaque iteration, affiche "Fizz" si le nombre est divisible par 3,
+// "Buzz" s'il est divisible par 5,et "FizzBuzz" si le nombre est divisible par 3 et 5. sinon, affiche simplement le nombre 
+console.log('-------exercice6-------')
 
-                            /*****exercice4*****/
-
-// ecrire une boucle for qui calcul la somme des nombres de 1 a 100 puis fiche le resultat dans la console
-let x = 100; 
-let somme = 0; 
-
-for (let i = 1; i <= x; i++) {
-  somme += i;
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0) {
+    console.log(i,"Fizz");
+  } else if (i % 5 === 0) {
+    console.log(i, "Buzz");
+  }  else if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i, "FizzBuzz");
+  }else {
+    console.log(i);
+  }
 }
-console.log(`La somme des nombres de 1 a ${x} est ${somme}`);
 
-                            /********exercice5********/
+
+
+
+
+
